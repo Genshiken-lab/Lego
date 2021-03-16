a4d23462dd17460b00b46378664c369c7fa959e9
Genshiken-lab <Genshikendruid@mail.ru@>
2021-03-17 01:57:11 +0400

    2

---
+++ src/Solution2.java
@@ -0,0 +1,9 @@
+public class Solution2 {
+    public static void main(String[] args) {
+        System.out.println("Элли очень умная");
+        System.out.println("Элли очень умная");
+        System.out.println("Элли очень умная");
+        System.out.println("Элли очень умная");
+        System.out.println("Элли очень умная");//напишите тут ваш код
+    }
+}

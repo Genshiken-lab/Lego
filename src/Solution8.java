1f5f6a1f9045b9e260ee4901887240fa6021d74b
Genshiken-lab <Genshikendruid@mail.ru@>
2021-04-03 22:26:38 +0400

    8

---
+++ src/Solution8.java
@@ -0,0 +1,8 @@
+public class Solution8 {
+    /*
+ Баги и фичи
+ */
+    public static void main(String[] args) {
+        System.out.println("Это не баг, это фича."); //напишите тут ваш код
+    }
+}

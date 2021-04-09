55796c1c8cff46166b651ee93085792e5ca973ba
Genshiken-lab <Genshikendruid@mail.ru@>
2021-04-09 20:26:18 +0400

    new comm

--- src/Bearsong.java
+++ src/Bearsong.java
@@ -1,16 +1,18 @@
 public class Bearsong {
+    //В коде есть одна ошибка. Он скомпилируется и запустится, но результат будет не таким,
+    // как ожидается. Поищите эту ошибку и попробуйте исправить
     public static void main(String[] args) {
         int beerNum = 99;
         String word = "бутылок (бутылки)";
         while (beerNum > 0) {
-            if (beerNum == 1) {
-                word = "бутылка";
-            }
             System.out.println(beerNum + " " + word + " пива на стене");
             System.out.println(beerNum + " " + word + " пива");
             System.out.println("Возьми одну.");
             System.out.println("Пусти по кругу.");
             beerNum = beerNum - 1;
+            if (beerNum == 1) {
+                word = "бутылка";
+            }
             if (beerNum > 0) {
                 System.out.println(beerNum + " " + word + " пива на стене");
             } else {

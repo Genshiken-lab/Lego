e65dc1309b17bedd7a7f443376449a162cb58c4f
Genshiken-lab <Genshikendruid@mail.ru@>
2021-04-26 01:39:34 +0400

    Scan2num

---
+++ src/G1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class G1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("получите квадрат числа 5");
+        int number = 5 * 5;
+        System.out.println("Вуаля " + number);
+    }
+}
+// неверно, нет ввода
---
+++ src/Sqr.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Sqr {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner (System.in);
+            int a = scan.nextInt();
+            System.out.println("Квадрат этого числа = "+(a*a));
+        }
+    }
+

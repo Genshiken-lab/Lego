f4faaf7fe194c14f3f6ede047a9f95ef9fc9e162
Genshiken-lab <Genshikendruid@mail.ru@>
2021-04-26 22:41:50 +0400

    Mod2

---
+++ src/Mod2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Mod2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        float f = scan.nextInt();
+        System.out.println("Модуль чиcла = "+ Math.abs(f));
+    }
+}
+
